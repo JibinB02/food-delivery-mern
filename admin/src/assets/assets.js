ec84e875219c8234dedc938bcb77804c9d38abbd
JibinB02 <98394194+JibinB02@users.noreply.github.com>
2024-08-20 02:01:29 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'http://food-delivery-backend-p55h.onrender.com'
+export const url = 'https://food-delivery-backend-p55h.onrender.com'

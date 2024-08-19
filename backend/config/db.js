a194f5730d44d759b165428b89f80a81440e0562
JibinB02 <jibinbabukp02@gmail.com>
2024-08-20 01:15:53 +0530

    "last_modified"

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,6 @@ import 'dotenv/config'
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL);
-    await mongoose.connect(process.env.MONGODB_URL);
     console.log("DB Connected");
   } catch (error) {
     console.error("DB Connection Error:", error);
